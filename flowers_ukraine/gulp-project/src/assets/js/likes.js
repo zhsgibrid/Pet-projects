132367b8a3c993c32b58b54b90331c3ed6b520fa
zhsgibrid <gibridzhs@gmail.com>
2022-08-27 21:59:46 +0300

    Add heart counter in header

--- flowers_ukraine/gulp-project/src/assets/js/likes.js
+++ flowers_ukraine/gulp-project/src/assets/js/likes.js
@@ -3,12 +3,15 @@ function like(heart_img){
     const like = './img/heart_liked.svg';
 
     let heart = document.getElementById('heart_num');
+    let heart_1 = document.getElementById('heart_num_1');
 
     if (heart_img.getAttribute('src') == unlike){
         heart_img.setAttribute('src', like);
         heart.textContent = parseInt(heart.textContent) + 1;
+        heart_1.textContent = parseInt(heart_1.textContent) + 1;
     } else{
         heart_img.setAttribute('src', unlike)
         heart.textContent = parseInt(heart.textContent) - 1;
+        heart_1.textContent = parseInt(heart_1.textContent) - 1;
     }
 }
